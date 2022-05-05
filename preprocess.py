6af6768dcca53a1215ce3dc9ee374d9d90e6b9a4
Brian <xiaoxiaokongxi@gmail.com>
2022-05-05 22:20:40 +0200

    save kpt finished

--- preprocess.py
+++ preprocess.py
@@ -1,17 +1,19 @@
 import matplotlib.pyplot as plt
 import face_alignment
 from skimage import io
+import numpy as np
 import os
 
 #load image folder file path
 imgfolder = 'G:\paper\Friends'
 kptfolder = 'G:\paper\Friends\kpt'
-segfolder = 'G:\paper\Friends\seg'
+
+# segfolder = 'G:\paper\Friends\seg'
 
 filename = os.listdir(imgfolder)
 for folder in filename:
     #get all name in filenames
-    path = os.path.abspath(os.path.join(imgfolder, folder))
+    path = os.path.abspath(os.path.join(imgfolder, folder))  # ../imagefolder/id_x
 
     #check is image folder
     if 'id_' in folder:
@@ -20,19 +22,20 @@ for folder in filename:
         if os.path.exists(os.path.join(kptfolder, folder)) == False:
             os.makedirs(os.path.join(kptfolder, folder))
             print('{} created'.format(os.path.join(kptfolder, folder)))
-        if os.path.exists(os.path.join(segfolder, folder)) == False:
-            os.makedirs(os.path.join(segfolder, folder))
-            print('{} created'.format(os.path.join(segfolder, folder)))
+
+        #if used for seg
+        # if os.path.exists(os.path.join(segfolder, folder)) == False:
+        #     os.makedirs(os.path.join(segfolder, folder))
+        #     print('{} created'.format(os.path.join(segfolder, folder)))
         
         # make prediction for kpt and save
         fan = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False)
         for imgfile in os.listdir(path):
             image = io.imread(os.path.join(path,imgfile))
             preds = fan.get_landmarks(image)
-            # refer to DECA
-            # landmarks2d = util.batch_orth_proj(landmarks2d, codedict['cam'])[:,:,:2]; landmarks2d[:,:,1:] = -landmarks2d[:,:,1:]#; landmarks2d = landmarks2d*self.image_size/2 + self.image_size/2
-            # np.savetxt(os.path.join(savefolder, name, name + '_kpt2d.txt'), opdict['landmarks2d'][0].cpu().numpy())
-
-
+            # print(preds)
+            if preds is not None:
+                np.savetxt(os.path.join(os.path.join(kptfolder, folder), imgfile[:-4] + '_kpt2d.txt'), preds[0])
+                print(os.path.join(os.path.join(kptfolder, folder), imgfile[:-4] + '_kpt2d.txt' + ' created'))
 
 
